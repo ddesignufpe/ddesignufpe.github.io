eb3191183230dac0578a065816502fe7ae2f4a8c
Jônathas Souza <jonathasbsouza@gmail.com>
2018-01-16 13:29:26 -0300

    json-based load

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,3 +1,51 @@
+/*Gerar cadeiras
+from http://github.com/caiobsouza */
+window.onload = function () {
+  var data = $.getJSON("./dist/js/cadeiras.json", function (data) {
+      for (var i in data) {
+          var el = document.getElementById(data[i].code);
+          if (el) {
+              var html = getDisciplina(data[i]);
+              el.innerHTML += html;
+          }
+      }
+  });
+
+}
+
+function getDisciplina(data) {
+  if(data.ementa !== null || data.ementa !== undefined ){
+    return `<div class="grupo ${data.eixo}">
+                <div class="codigo">
+                    <span>${data.code}</span><span>${data.siga}</span>
+                </div>
+                <div class="conteudo">
+                    <h4>${data.name}</h4>
+                    <p class="professor">${data.teacher}</p>
+                    <p class="ciclo">Ciclo ${data.ciclo}</p>
+                    <p class="vagas">${data.vagas} vagas</p>
+                    <p class="local">${data.local}</p>
+                    <a href="http://${data.ementa}" target="_blank">
+                        <button>Ementa</button>
+                    </a>
+                </div>
+            </div>`;
+} else {
+    return `<div class="grupo obrigatorio">
+                <div class="codigo">
+                    <span>${data.code}</span>
+                </div>
+                <div class="conteudo">
+                <h4>${data.name}</h4>
+                <p class="professor">${data.teacher}</p>
+                <p class="ciclo">${data.ciclo}</p>
+                <p class="vagas">${data.vagas} vagas</p>
+                <p class="local">${data.local}</p>
+                </div>
+            </div>`;
+}
+}
+
 /* Tabs
 from https://codepen.io/tgallimore/pen/awGzD */
 $('.tabgroup > div').hide();
