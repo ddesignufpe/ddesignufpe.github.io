3a41ea14fc534e735d22f16c97b353427ebef33e
Jônathas Souza <jonathasbsouza@gmail.com>
2018-03-24 12:08:58 -0300

    nav // scroll to top

--- user/dist/js/scripts.js
+++ user/dist/js/scripts.js
@@ -1,51 +1,3 @@
-/*Gerar cadeiras
-from http://github.com/caiobsouza */
-window.onload = function () {
-  var data = $.getJSON("./dist/js/cadeiras.json", function (data) {
-      for (var i in data) {
-          var el = document.getElementById(data[i].code);
-          if (el) {
-              var html = getDisciplina(data[i]);
-              el.innerHTML += html;
-          }
-      }
-  });
-
-}
-
-function getDisciplina(data) {
-  if(data.ementa !== ""){
-    return `<div class="grupo ${data.eixo}">
-                <div class="codigo">
-                    <span>${data.code}</span><span>${data.siga}</span>
-                </div>
-                <div class="conteudo">
-                    <h4>${data.name}</h4>
-                    <p class="professor">${data.teacher}</p>
-                    <p class="ciclo">Ciclo ${data.ciclo}</p>
-                    <p class="vagas">${data.vagas} vagas</p>
-                    <p class="local">${data.local}</p>
-                    <a href="ementas/${data.ementa}.pdf" target="_blank">
-                        <button>Ementa</button>
-                    </a>
-                </div>
-            </div>`;
-} else {
-    return `<div class="grupo ${data.eixo}">
-                <div class="codigo">
-                    <span>${data.code}</span><span>${data.siga}</span>
-                </div>
-                <div class="conteudo">
-                <h4>${data.name}</h4>
-                <p class="professor">${data.teacher}</p>
-                <p class="ciclo">${data.ciclo}</p>
-                <p class="vagas">${data.vagas} vagas</p>
-                <p class="local">${data.local}</p>
-                </div>
-            </div>`;
-}
-}
-
 /* Tabs
 from https://codepen.io/tgallimore/pen/awGzD */
 $('.tabgroup > div').hide();
@@ -82,4 +34,25 @@ jQuery(function($) {
   }
   $(window).scroll(fixDiv);
   fixDiv();
+});
+
+// Scroll to Top
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 300);
+        return false;
+    });
+
 });
