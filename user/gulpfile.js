0dead67641d3b3bf5533e4894f2e5334fec75759
Jônathas Souza <jonathasbsouza@gmail.com>
2018-03-23 23:50:47 -0300

    mudança gulpfile sass + browsersync

--- user/gulpfile.js
+++ user/gulpfile.js
@@ -1,15 +1,24 @@
-'use strict';
  
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+//const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
  
 gulp.task('sass', function () {
   return gulp.src('./src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+  .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(gulp.dest('./dist/css'))
+  .pipe(browserSync.stream());
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/scss/**/*.scss', ['sass']);
+gulp.task('serve', function () {
+  
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("./src/scss/**/*.scss", ['sass']);  
+  gulp.watch("./dist/**/*.scss").on('change', browserSync.reload);
+  gulp.watch("./**/*.html").on('change', browserSync.reload);
+  gulp.watch("./dist/**/*.js").on('change', browserSync.reload);
 });
